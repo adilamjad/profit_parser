cab45ce11a798eb2098f82a297a5f4cddac631b3
Adil Amjad <adilamjad@gmail.com>
2020-04-19 20:59:22 +0500

    Comparison log files

---
+++ COMPARISON/INCREASE_VEHICLES/run_parser-no.sh
@@ -0,0 +1,6 @@
+rm row*
+
+./profit_parser rate-10-nodes-20-no-profit.log 1 1 13
+./profit_parser rate-10-nodes-30-no-profit.log 1 1 13
+./profit_parser rate-10-nodes-40-no-profit.log 1 1 13
+./profit_parser rate-10-nodes-50-no-profit.log 1 1 13
---
+++ COMPARISON/INCREASE_VEHICLES/run_parser.sh
@@ -0,0 +1,6 @@
+rm row*
+
+./profit_parser rate-10-nodes-20-profit.log 1 1 13
+./profit_parser rate-10-nodes-30-profit.log 1 1 13
+./profit_parser rate-10-nodes-40-profit.log 1 1 13
+./profit_parser rate-10-nodes-50-profit.log 1 1 13
